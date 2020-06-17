b6f4fe17dd44d8418ce4230e1a8c8c696500019a
Achromia <Achromia@users.noreply.github.com>
2020-06-17 00:13:27 -0700

    CharacterMovement
    Created character movement and a starting platform/

---
+++ Platformer/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
