1b1a5e97948866ed4c3fe3cfc5cadfc71790a316
Calvin Yoh <flashfire340@gmail.com>
2020-06-18 01:48:48 -0700

    Main Menu Creation
    Created Main menu with Start and Level Select

---
+++ Platformer/Assets/Scripts/MainMenu/MainMenuScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject levelSelectCanvas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenLevelSelect()
+    {
+        levelSelectCanvas.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
