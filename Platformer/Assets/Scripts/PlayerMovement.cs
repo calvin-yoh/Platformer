6e652674695da86a5c28e2aceacdcf6193d7dc37
Achromia <Achromia@users.noreply.github.com>
2020-06-19 02:19:37 -0700

    Fixed Player Movement
    Bunny hops boi.

--- Platformer/Assets/Scripts/PlayerMovement.cs
+++ Platformer/Assets/Scripts/PlayerMovement.cs
@@ -2,24 +2,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//わたしのともだちはばかです。
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
     float speed = 5.0f;
 
+    public Vector2 jump;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0);
         transform.position += move * speed * Time.deltaTime;
+
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.UpArrow))  //makes player jump
+        {
+            GetComponent<Rigidbody2D>().AddForce(jump, ForceMode2D.Impulse);
+        }
     }
 
     public void SetSpeed(float newSpeed)
@@ -27,4 +35,3 @@ public class PlayerMovement : MonoBehaviour
         speed = newSpeed;
     }
 }
-    //わたしのともだちはばかです。
