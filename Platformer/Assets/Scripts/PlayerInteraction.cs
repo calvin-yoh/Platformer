e9b0464386e878fbcbb9fa963a18eb4f0dd26d7e
Calvin Yoh <flashfire340@gmail.com>
2020-06-22 15:16:53 -0700

    Introduced Coins
    Added a coin object and made it responsive to external stimulus

---
+++ Platformer/Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerInteraction : MonoBehaviour
+{
+
+    [SerializeField] private CharacterController2D controller;
+    [SerializeField] private PlayerMovement movement;
+    [SerializeField] private Text winText;
+    [SerializeField] private Vector3 startingPos;
+
+    private void Awake()
+    {
+        winText.enabled = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        //checks if at goal
+        if (trigger.gameObject.tag == "Goal")
+        {
+            movement.enabled = false;
+            controller.Move(0, false, false);
+            winText.enabled = true;
+        }
+
+        //check if respawn
+        if (trigger.gameObject.tag == "Boundary")
+        {
+            this.transform.position = startingPos;
+        }
+
+        if (trigger.gameObject.tag == "Coin")
+        {
+            trigger.gameObject.SetActive(false);
+            Debug.Log("Score Increased");
+        }
+    }
+}
--- Platformer/Assets/Scripts/PlayerMovement.cs
+++ Platformer/Assets/Scripts/PlayerMovement.cs
@@ -9,18 +9,11 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private CharacterController2D controller;
     [SerializeField] private float runSpeed = 40f;
-    [SerializeField] private Text winText;
-    [SerializeField] private Vector3 startingPos;
+    
 
     private float horizontalMove = 0f;
     private bool jump = false;
     private bool crouch = false;
-
-
-    private void Awake()
-    {
-        winText.enabled = false;
-    }
 
     // Update is called once per frame
     void Update()
@@ -45,23 +38,5 @@ public class PlayerMovement : MonoBehaviour
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
-    }
-
-    void OnTriggerEnter2D(Collider2D trigger)
-    { 
-        //checks if at goal
-        if (trigger.gameObject.tag == "Goal")
-        {
-            horizontalMove = 0;
-            controller.Move(0, false, false);
-            winText.enabled = true;
-            this.enabled = false;
-        }
-
-        //check if respawn
-        if (trigger.gameObject.tag == "Boundary")
-        {
-            this.transform.position = startingPos;
-        }
-    }
+    }  
 }
